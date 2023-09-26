1ab98ba1fb9477476df9bd73f886f5b80931fef3
Tyler Leslie <tylerleslie22@augustana.edu>
2023-09-26 13:06:23 -0500

    "Now with dancing chicken"

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -2,6 +2,10 @@ package edu.augustana;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello bird git world!");
+
+
+        System.out.println("Hello bird world!");
+        Chicken doug = new Chicken();
+        doug.dance();
     }
 }
